cd34634a235ad4f49842db39834d7ca6aed986d7
EkaterinaBelonosova <33663086+EkaterinaBelonosova@users.noreply.github.com>
2019-06-05 17:57:00 +0700

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -1,12 +1,39 @@
 /* Article FructCode.com */
 $( document ).ready(function() {
+	let resp = '[{"required":true,"fields":{"NAME":false}},{"required":false,"fields":{"LAST_NAME":true}},{"required":true,"fields":{"EMAIL":false,"PHONE":false}},{"required":false,"fields":{"TYPE":false}},{"required":false,"fields":{"TEXT":false}}]';
+	let js = JSON.parse(resp);
+	let js_lenght = js.length;
+	console.log(js[0]);
+	console.log(js[0].fields);
+
+
+
+		
+		
+			for(var key in js){
+				for(let i=0; i<js.length; i++){				
+					if(i==key){
+						for(var k in key){
+						console.log(js[i].fields);
+						console.log(k);
+							$('<div></div><br>', {
+								style: js[i].fields.data 
+							}).appendTo('#form');
+							$('<div style="display:inline-block"></div><br>', {
+							}).appendTo('div');
+								if(js[i].required==true){
+									
+								}
+						}
+					}	
+				}
+				
+			}
+	
+			console.log(js.length);
+			
     $("#btn").click(	
 		function(){
-			let resp = '[{"required":true,"fields":{"NAME":false}},{"required":false,"fields":{"LAST_NAME":true}},{"required":true,"fields":{"EMAIL":false,"PHONE":false}},{"required":false,"fields":{"TYPE":false}},{"required":false,"fields":{"TEXT":false}}]';
-			let js = JSON.parse(resp);
-			console.log(js.length);
-			console.log(js[0].fields.NAME);
-			console.log(js);
 			let $data={};
 			$('#ajax_form').find ('input').each(function() {
 					  // добавим новое свойство к объекту $data
@@ -14,18 +41,11 @@ $( document ).ready(function() {
 					  // значение свойства – значение свойство value элемента
 					  $data[this.name] = $(this).val();
 					});
-			console.log($data);	
+			/*console.log($data);*/	
 			for (var key in $data) {
-				  console.log(key);
+				  /*console.log(key);*/
 				}
-			for(let i=0; i<js.length; i++){
-				for(var key in $data){
-					if(js[i].fields==key){
-						if(js[i].required==true)
-					}	
-				}
-				
-			}
+			
 			sendAjaxForm('result_form', 'ajax_form', 'https://workspace.ru/ajax/test/test.php');
 			return false; 
 		}
@@ -46,8 +66,14 @@ console.log($("#"+ajax_form).serialize());
 		data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
         success: function(response) { //Данные отправлены успешно
         	result = $.parseJSON(response);
-        	$('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber+result.last_name+result.email);
-			console.log(result);
+			$("p").empty();
+			for (var key in result.data) {
+				if(key == "message"){
+					$('#result_form').html('Ответ '+result.data[key]);
+				}else{
+					$("#"+key).html('Ответ '+result.data[key]);
+				}
+			}
 			
     	},
     	error: function(response) { // Данные не отправлены
@@ -57,15 +83,3 @@ console.log($("#"+ajax_form).serialize());
 		
  	});
 }
-
- 
-/*[{
-	"required":true,
-	"fields":{
-		"NAME":false
-		}
-	},
-{"required":false,"fields":{"LAST_NAME":true}},
-{"required":true,"fields":{"EMAIL":false,"PHONE":false}},
-{"required":false,"fields":{"TYPE":false}},
-{"required":false,"fields":{"TEXT":false}}]*/
